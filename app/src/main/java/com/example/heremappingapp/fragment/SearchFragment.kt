23d37f0dea067686d7f39586125065154b59551f
MinhLe2512 <63902542+MinhLe2512@users.noreply.github.com>
2021-09-22 20:54:19 +0700

    AutoComplete Search with RecyclerView

--- app/src/main/java/com/example/heremappingapp/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/heremappingapp/fragment/SearchFragment.kt
@@ -3,9 +3,12 @@ package com.example.heremappingapp.fragment
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.heremappingapp.R
 import com.example.heremappingapp.`class`.UserLocation
 import com.example.heremappingapp.databinding.FragmentSearchBinding
@@ -20,7 +23,7 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
     private var searchEngine: SearchEngine? = null
 
     private val maxItems = 30
-    private val maxSuggestedItems = 5
+    private val maxSuggestedItems = 15
     private var centerGeoCoordinates: GeoCoordinates? = null
     private var searchOptions: SearchOptions? = null
     override fun onCreateView(
@@ -35,13 +38,13 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
     }
 
     private fun handleBackButton() {
-        binding!!.imgBtn.setOnClickListener{
+        binding!!.imgBtn.setOnClickListener {
             (activity as AppCompatActivity).supportFragmentManager.beginTransaction().remove(this).commit()
         }
     }
 
     private fun initSearch() {
-        try{
+        try {
             searchEngine = SearchEngine()
         } catch (e: InstantiationErrorException) {
             throw RuntimeException("Initialization of SearchEngine failed: " + e.error.name)
@@ -49,6 +52,12 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         searchOptions = SearchOptions(LanguageCode.EN_US, maxSuggestedItems)
         val userLocation = arguments?.getSerializable("user_location") as UserLocation
         centerGeoCoordinates = userLocation.centerGeoCoordinates
+    }
+
+    private fun initRecyclerView(list: MutableList<Place>) {
+        binding!!.searchRes.layoutManager = LinearLayoutManager(activity)
+        val adapter = ResultSearchAdapter(list)
+        binding!!.searchRes.adapter = adapter
     }
 
     private fun handleSearch() {
@@ -61,7 +70,9 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
 
             override fun onQueryTextChange(newText: String?): Boolean {
                 searchEngine!!.suggest(
-                    TextQuery(newText!!, centerGeoCoordinates!!), searchOptions!!, autoSuggestCallback
+                    TextQuery(newText!!, centerGeoCoordinates!!),
+                    searchOptions!!,
+                    autoSuggestCallback
                 )
                 return true
             }
@@ -69,31 +80,54 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         })
     }
 
-    private final var autoSuggestCallback = SuggestCallback(){ searchError: SearchError?, mutableList: MutableList<Suggestion>? ->
-        if (searchError != null) {
-            Toast.makeText(activity, searchError.name, Toast.LENGTH_LONG).show()
-            return@SuggestCallback
-        }
-        if (mutableList != null) {
-            Log.d("LOG_TAG", "Autosuggest results: " + mutableList.size)
-        }
+    private final var autoSuggestCallback =
+        SuggestCallback { searchError: SearchError?, mutableList: MutableList<Suggestion>? ->
+            if (searchError != null) {
+                Log.d("Search_error", searchError.name)
+                return@SuggestCallback
+            }
 
-        if (mutableList != null) {
-            for (autoSuggestResult in mutableList) {
-                var addressText = "Not a place."
-                val place = autoSuggestResult.place
-                if (place != null) {
-                    addressText = place.address.addressText
+            if (mutableList != null) {
+                val list =  mutableListOf<Place>()
+                for (suggestion in mutableList) {
+                    val place = suggestion.place
+                    if (place != null) {
+                        list.add(place)
+                    }
+                    else continue
                 }
-                Log.d(
-                    "LOG_TAG", "Autosuggest result: " + autoSuggestResult.title +
-                            " addressText: " + addressText
-                )
+                initRecyclerView(list)
+                return@SuggestCallback
             }
         }
-    }
+
     override fun onDestroy() {
         super.onDestroy()
         binding = null
     }
+
+    private inner class ResultSearchAdapter(private val list: MutableList<Place>) :
+        RecyclerView.Adapter<ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_search_result, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val itemsViewSearchRes = list[position]
+            holder.titleView.text = itemsViewSearchRes.title
+            holder.addressView.text = itemsViewSearchRes.address.addressText
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+    }
+
+    private inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleView: TextView = view.findViewById(R.id.txt_title)
+        val addressView: TextView = view.findViewById(R.id.txt_address)
+    }
 }
+
+
